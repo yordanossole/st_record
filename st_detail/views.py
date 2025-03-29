19157a13181ef31c1d42c7fe21eefbbc8a6e9726
yordanossole <yordanos.sole@gmail.com>
2025-03-29 12:15:58 +0300

    Internal server error handled, 404 response for unavailable pages, CORS enabled for all sites

--- st_detail/views.py
+++ st_detail/views.py
@@ -18,30 +18,54 @@ from .models import Person
 
 class PersonList(APIView):
     def get(self, request):
-        queryset = Person.objects.all()
-        serializer = PersonSerializer(queryset, many=True)
-        return Response(serializer.data)
+        try:
+            queryset = Person.objects.all()
+            serializer = PersonSerializer(queryset, many=True)
+            return Response(serializer.data)
+        except Exception:
+            return Response({'error': 'Something went wrong.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def post(self, request):
-        serializer = PersonSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
+        try:
+            serializer = PersonSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response({'error': 'Something went wrong.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 class PersonDetail(APIView):
     def get(self, request, id):
-        person = get_object_or_404(Person, pk=id)
-        serializer = PersonSerializer(person)
-        return Response(serializer.data)
+        try:
+            person = get_object_or_404(Person, pk=id)
+            serializer = PersonSerializer(person)
+            return Response(serializer.data)
+        except Exception:
+            return Response({'error': 'Something went wrong.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def put(self, request, id):
-        person = get_object_or_404(Person, pk=id)
-        serializer = PersonSerializer(person, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        try:
+            person = get_object_or_404(Person, pk=id)
+            serializer = PersonSerializer(person, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        except Exception:
+            return Response({'error': 'Something went wrong.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def delete(self, request, id):
-        person = get_object_or_404(Person, pk=id)
-        person.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            person = get_object_or_404(Person, pk=id)
+            person.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception:
+            return Response({'error': 'Something went wrong.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class NotFound(APIView):
+    def get(self, request):
+        return Response({'error': "Page not found."}, status=status.HTTP_404_NOT_FOUND)
+    def post(self, request):
+        return Response({'error': "Page not found."}, status=status.HTTP_404_NOT_FOUND)
+    def put(self, request):
+        return Response({'error': "Page not found."}, status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request):
+        return Response({'error': "Page not found."}, status=status.HTTP_404_NOT_FOUND)    
--- st_record/urls.py
+++ st_record/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from st_detail import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('person/', include('st_detail.urls')),
+    re_path(r'.*', views.NotFound.as_view())
 ]
